41a10bc2491e6fa3360cb53a771eedd580ffaec7
Tim Smith <tim@timothybsmith.com>
2013-05-26 19:25:54 -0500

    Changed to new domain

--- themes/ag_v4/index.php
+++ themes/ag_v4/index.php
@@ -2,9 +2,9 @@
 
 <section class="main-content" role="main">
   <div class="contain">
-    <h1>Anythin’ Goes is the small design studio of <a href="http://timothybsmith.com" target="_blank">Tim Smith</a>.<br>I design experiences for the web.</h1>
+    <h1>Anythin’ Goes is the small design studio of <a href="http://ttimsmith.com" target="_blank">Tim Smith</a>.<br>I design experiences for the web.</h1>
 
-    <p>Based in St. Paul, Minnesota, I design for clients of all types, from small businesses to large companies, from editorial sites to personal ones. You can take a look at my work in <a href="http://timothybsmith.com/portfolio" target="_blank">my online portfolio</a>.</p>
+    <p>Based in St. Paul, Minnesota, I design for clients of all types, from small businesses to large companies, from editorial sites to personal ones. You can take a look at my work in <a href="http://ttimsmith.com/portfolio" target="_blank">my online portfolio</a>.</p>
 
     <p>I also <a href="http://timlikestoteach.com" target="_blank">teach web design</a>, front-end development, and more. I’d be happy to talk to you about running a workshop or training at your company.</p>
     
